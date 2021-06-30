559cc50faa63e0c20c0490caaaad13b09cd8aa3c
fixyo <beachboy@yeah.net>
2021-06-30 23:26:08 +0800

    add: use fetch

---
+++ src/utils/request.ts
@@ -0,0 +1,41 @@
+import qs from "qs";
+import * as auth from "utils/auth-provider";
+const baseUrl = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+export const request = (
+  path: string,
+  { data, token, headers, ...others }: Config
+) => {
+  const config = {
+    method: "GET",
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...others,
+  };
+  if (config.method.toUpperCase() === "GET") {
+    path += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+  return fetch(`${baseUrl}${path}`, config).then(async (res) => {
+    if (res.status === 401) {
+      auth.logOut();
+      window.location.reload();
+      return Promise.reject({ msg: "need login" });
+    }
+
+    const data = await res.json();
+
+    if (res.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
+};
